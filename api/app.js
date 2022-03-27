f4df861a1f8b932522b88cbcc879b63c6158e348
Ha Quoc <havuongquoc2000@gmail.com>
2022-03-27 21:29:12 +0700

    27/03

--- api/app.js
+++ api/app.js
@@ -15,11 +15,26 @@ const { ethers } = require("hardhat");
 // });
 
 (async () => {
-  const owner = await ethers.getSigners();
+  const owners = await ethers.getSigners();
   const token = await ethers.getContractFactory("TokenWDT");
   const contract = await token.deploy();
-  const name = await contract.balanceOf(owner[0].address);
-  console.log(contract.address);
+
+  // console.log(owners[0]);
+  for (owner of owners) {
+    console.log(parseInt(await owner.getBalance()));
+  }
+  const tx = await owners[0].sendTransaction({
+    to: owners[1].address,
+    value: ethers.utils.parseEther("5.0"),
+  });
+  console.log(
+    "after sending",
+    parseInt(await owners[0].getBalance()),
+    parseInt(await owners[1].getBalance())
+  );
+
+  // const name = await contract.balanceOf(owner[0].address);
+  // console.log(111111, await contract.address);
 })();
 
 // app.listen(3000, () => console.log("App is listen on ", 3000));
